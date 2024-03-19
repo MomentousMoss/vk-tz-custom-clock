3847cff39b21578647528846df10bfcf1cbc0c03
Momentous Moss <momentousmoss@gmail.com>
2024-03-19 19:30:27 +0300

    vk-2 numbers, clockFace

--- app/src/main/java/com/momentousmoss/vk_tz_custom_clock/CustomClock.kt
+++ app/src/main/java/com/momentousmoss/vk_tz_custom_clock/CustomClock.kt
@@ -7,150 +7,127 @@ import android.graphics.Paint
 import android.graphics.drawable.Drawable
 import android.text.TextPaint
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
+import kotlin.math.cos
+import kotlin.math.sin
 
-/**
- * TODO: document your custom view class.
- */
-class CustomClock : View {
+const val FPS : Long = 1000 / 60
+const val DEFAULT_FACE_RADIUS = 0f
+const val DEFAULT_TEXT_DRAW = false
+const val DEFAULT_TEXT_PADDING = 0f
+const val DEFAULT_TEXT_COLOR = Color.BLACK
+const val DEFAULT_TEXT_SIZE = 12f
+const val DEFAULT_TEXT_SIZE_CENTER_OFFSET_MULTIPLIER = 0f
+const val DEFAULT_HAND_LENGTH = -2f
+const val DEFAULT_HAND_WIDTH = -2f
 
-    private var _exampleString: String? = null // TODO: use a default from R.string...
-    private var _exampleColor: Int = Color.RED // TODO: use a default from R.color...
-    private var _exampleDimension: Float = 0f // TODO: use a default from R.dimen...
+const val TEXT_CENTER_OFFSET_MULTIPLIER = 10f
 
-    private lateinit var textPaint: TextPaint
-    private var textWidth: Float = 0f
-    private var textHeight: Float = 0f
+class CustomClock @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : View(context, attrs, defStyle) {
 
-    /**
-     * The text to draw
-     */
-    var exampleString: String?
-        get() = _exampleString
-        set(value) {
-            _exampleString = value
-            invalidateTextPaintAndMeasurements()
-        }
+    private var viewWidth: Int = 0
+    private var viewHeight: Int = 0
 
-    /**
-     * The font color
-     */
-    var exampleColor: Int
-        get() = _exampleColor
-        set(value) {
-            _exampleColor = value
-            invalidateTextPaintAndMeasurements()
-        }
+    var clockFaceRadius: Float = DEFAULT_FACE_RADIUS
+    var clockFaceDrawable: Drawable? = null
 
-    /**
-     * In the example view, this dimension is the font size.
-     */
-    var exampleDimension: Float
-        get() = _exampleDimension
-        set(value) {
-            _exampleDimension = value
-            invalidateTextPaintAndMeasurements()
-        }
+    var numberTextPaint: TextPaint
 
-    /**
-     * In the example view, this drawable is drawn above the text.
-     */
-    var exampleDrawable: Drawable? = null
+    var numberTextDraw: Boolean = DEFAULT_TEXT_DRAW
+    var numberTextPadding: Float = DEFAULT_TEXT_PADDING
+    var numberTextColor: Int = DEFAULT_TEXT_COLOR
+    var numberTextSize: Float = DEFAULT_TEXT_SIZE
+    var numberTextSizeCenterOffsetMultiplier : Float = DEFAULT_TEXT_SIZE_CENTER_OFFSET_MULTIPLIER
 
-    constructor(context: Context) : super(context) {
-        init(null, 0)
-    }
+    var secondsHandLength: Float = DEFAULT_HAND_LENGTH
+    var secondsHandWidth: Float = DEFAULT_HAND_WIDTH
 
-    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-        init(attrs, 0)
-    }
+    var minutesHandLength: Float = DEFAULT_HAND_LENGTH
+    var minutesHandWidth: Float = DEFAULT_HAND_WIDTH
 
-    constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(
-        context,
-        attrs,
-        defStyle
-    ) {
-        init(attrs, defStyle)
-    }
+    var hoursHandLength: Float = DEFAULT_HAND_LENGTH
+    var hoursHandWidth: Float = DEFAULT_HAND_WIDTH
 
-    private fun init(attrs: AttributeSet?, defStyle: Int) {
-        // Load attributes
-        val a = context.obtainStyledAttributes(
+    init {
+        context.obtainStyledAttributes(
             attrs, R.styleable.CustomClock, defStyle, 0
-        )
+        ).apply {
+            clockFaceRadius = getDimension(R.styleable.CustomClock_clockFaceRadius, DEFAULT_FACE_RADIUS)
+            clockFaceDrawable = getDrawable(R.styleable.CustomClock_clockFaceDrawable)
 
-        _exampleString = a.getString(
-            R.styleable.CustomClock_exampleString
-        )
-        _exampleColor = a.getColor(
-            R.styleable.CustomClock_exampleColor,
-            exampleColor
-        )
-        // Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
-        // values that should fall on pixel boundaries.
-        _exampleDimension = a.getDimension(
-            R.styleable.CustomClock_exampleDimension,
-            exampleDimension
-        )
+            numberTextDraw = getBoolean(R.styleable.CustomClock_numberTextDraw, DEFAULT_TEXT_DRAW)
+            numberTextPadding = getDimension(R.styleable.CustomClock_numberTextPadding, DEFAULT_TEXT_PADDING)
+            numberTextColor = getColor(R.styleable.CustomClock_numberTextColor, DEFAULT_TEXT_COLOR)
+            numberTextSize = getDimension(R.styleable.CustomClock_numberTextSize, DEFAULT_TEXT_SIZE)
+            numberTextSizeCenterOffsetMultiplier = TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP, numberTextSize, context.resources.displayMetrics
+            ) / TEXT_CENTER_OFFSET_MULTIPLIER
 
-        if (a.hasValue(R.styleable.CustomClock_exampleDrawable)) {
-            exampleDrawable = a.getDrawable(
-                R.styleable.CustomClock_exampleDrawable
-            )
-            exampleDrawable?.callback = this
+            secondsHandLength = getDimension(R.styleable.CustomClock_secondsHandLength, DEFAULT_HAND_LENGTH)
+            secondsHandWidth = getDimension(R.styleable.CustomClock_secondsHandWidth, DEFAULT_HAND_WIDTH)
+
+            minutesHandLength = getDimension(R.styleable.CustomClock_minutesHandLength, DEFAULT_HAND_LENGTH)
+            minutesHandWidth = getDimension(R.styleable.CustomClock_minutesHandWidth, DEFAULT_HAND_WIDTH)
+
+            hoursHandLength = getDimension(R.styleable.CustomClock_hoursHandLength, DEFAULT_HAND_LENGTH)
+            hoursHandWidth = getDimension(R.styleable.CustomClock_hoursHandWidth, DEFAULT_HAND_WIDTH)
+
+            recycle()
         }
 
-        a.recycle()
 
-        // Set up a default TextPaint object
-        textPaint = TextPaint().apply {
+        numberTextPaint = TextPaint().apply {
             flags = Paint.ANTI_ALIAS_FLAG
             textAlign = Paint.Align.LEFT
-        }
-
-        // Update TextPaint and text measurements from attributes
-        invalidateTextPaintAndMeasurements()
-    }
-
-    private fun invalidateTextPaintAndMeasurements() {
-        textPaint.let {
-            it.textSize = exampleDimension
-            it.color = exampleColor
-            textWidth = it.measureText(exampleString)
-            textHeight = it.fontMetrics.bottom
+            textSize = numberTextSize
+            color = numberTextColor
         }
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        // TODO: consider storing these as member variables to reduce
-        // allocations per draw cycle.
-        val paddingLeft = paddingLeft
-        val paddingTop = paddingTop
-        val paddingRight = paddingRight
-        val paddingBottom = paddingBottom
+        drawClockFace(canvas)
+        drawNumbers(canvas)
 
-        val contentWidth = width - paddingLeft - paddingRight
-        val contentHeight = height - paddingTop - paddingBottom
+        postInvalidateDelayed(FPS)
+    }
 
-        exampleString?.let {
-            // Draw the text.
-            canvas.drawText(
-                it,
-                paddingLeft + (contentWidth - textWidth) / 2,
-                paddingTop + (contentHeight + textHeight) / 2,
-                textPaint
-            )
-        }
-
-        // Draw the example drawable on top of the text.
-        exampleDrawable?.let {
+    private fun drawClockFace(canvas: Canvas) {
+        clockFaceDrawable?.let {
             it.setBounds(
-                paddingLeft, paddingTop,
-                paddingLeft + contentWidth, paddingTop + contentHeight
+                0, 0,
+                viewWidth, viewHeight
             )
             it.draw(canvas)
+        }
+    }
+
+    private fun drawNumbers(canvas: Canvas) {
+        if (numberTextDraw) {
+            for (number in 1..12) {
+                val angle = Math.toRadians((30 * (number - 3)).toDouble())
+                val radiusWithTextPadding = clockFaceRadius  - numberTextPadding
+                val x = (viewWidth / 2 + cos(angle) * radiusWithTextPadding).toFloat()
+                val y = (viewHeight / 2 + sin(angle) * radiusWithTextPadding).toFloat()
+                val textSizeCenterOffset = numberTextSizeCenterOffsetMultiplier * number.toString().length
+                canvas.drawText(number.toString(), x - textSizeCenterOffset, y + textSizeCenterOffset, numberTextPaint)
+            }
+        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        val viewSize = if (h > w) w else h
+        super.onSizeChanged(viewSize, viewSize, oldw, oldh)
+        viewWidth = viewSize
+        viewHeight = viewSize
+        if (clockFaceRadius == DEFAULT_FACE_RADIUS) {
+            clockFaceRadius = viewSize / 2f
         }
     }
 }
